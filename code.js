182b6840c76cbd1bc587f3f11f6736ef680f5374
Ivan Martin <69719490+DojeeDev@users.noreply.github.com>
2025-03-28 17:20:03 -0600

    code works

--- code.js
+++ code.js
@@ -1,14 +1,10 @@
-// may be helpful : 
-// https://www.maxgcoding.com/in-place-merge-sorting
 function mergesort(arr) {
-  console.log(arr.length);
   for (var j = 1; j < arr.length; j *= 2) { 
-    console.log(j + " => " + (j*2));
     for (var m = 0; m < arr.length-1; m += (2*j)) {
       var start = m;
       var end = Math.min(m + j, arr.length-1);
 
-      while (start < end && end < Math.min((m + (2*j), arr.length)) ) {
+      while (start < end && end < (Math.min((m + (2*j)), arr.length)) )  {
         // compare
         if ( arr[start] <= arr[end] ) {
           start += 1;
@@ -21,6 +17,7 @@ function mergesort(arr) {
             arr[s] = arr[s-1];
           }
           arr[start+1] = tmp;
+          start++;
           end++;
         }
       }
@@ -28,19 +25,3 @@ function mergesort(arr) {
   }
   return arr;
 }
-
-function isSorted(a) {
-  for (var i = 1; i < a.length; i++) {
-    if (a[i-1] > a[i]) { 
-      console.log(a[i-1] + " > " + a[i]);
-      return false; 
-    }
-  }
-  return true;
-}
-
-var a = Array.from({length: 32}, () => Math.floor(Math.random() * 40));
-console.log(a);
-console.log(mergesort(a));
-if (!isSorted(a)) { console.log("NOT SORTED RIGHT") };
-console.log(mergesort([2,4,2,4,1,3,8,]));
